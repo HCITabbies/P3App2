a5183277368e9b8f159299c38aede2f408010876
Jack <jack.m.graham.iii.gr@dartmouth.edu>
2017-05-14 00:48:31 -0400

    Added login screen

---
+++ app/src/main/java/com/p3app2/LoginActivity.java
@@ -0,0 +1,147 @@
+package com.p3app2;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private static final String TAG = "LoginActivity";
+    private static final int REQUEST_SIGNUP = 0;
+
+
+    protected EditText email_text;
+    protected EditText passwd_text;
+    protected Button login_btn;
+    protected TextView signup_txt;
+
+//    @InjectView(R.id.input_email) EditText email_text;
+//    @InjectView(R.id.input_password) EditText passwd_text;
+//    @InjectView(R.id.btn_login) Button login_btn;
+//    @InjectView(R.id.link_signup) TextView signup_txt;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        email_text = (EditText) findViewById(R.id.input_email);
+        passwd_text = (EditText) findViewById(R.id.input_password);
+        login_btn = (Button) findViewById(R.id.btn_login);
+        signup_txt = (TextView) findViewById(R.id.link_signup);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        signup_txt.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity
+                //Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                //startActivityForResult(intent, REQUEST_SIGNUP);
+            }
+        });
+    }
+
+    public void login() {
+        Log.d(TAG, "Login");
+
+        if (!validate()) {
+            onLoginFailed();
+            return;
+        }
+
+        login_btn.setEnabled(false);
+
+        final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this,
+                R.style.AppTheme_Dark_Dialog);
+        progressDialog.setIndeterminate(true);
+        progressDialog.setMessage("Authenticating...");
+        progressDialog.show();
+
+        String email = email_text.getText().toString();
+        String password = passwd_text.getText().toString();
+
+        // TODO: Implement your own authentication logic here.
+
+        new android.os.Handler().postDelayed(
+                new Runnable() {
+                    public void run() {
+                        // On complete call either onLoginSuccess or onLoginFailed
+                        onLoginSuccess();
+                        // onLoginFailed();
+                        progressDialog.dismiss();
+                    }
+                }, 2000);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_SIGNUP) {
+            if (resultCode == RESULT_OK) {
+
+                // TODO: Implement successful signup logic here
+                // By default we just finish the Activity and log them in automatically
+                this.finish();
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // disable going back to the MainActivity
+        moveTaskToBack(true);
+    }
+
+    public void onLoginSuccess() {
+        login_btn.setEnabled(true);
+
+        Intent intent = new Intent(this, WelcomeActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onLoginFailed() {
+        Toast.makeText(getBaseContext(), "Login failed", Toast.LENGTH_LONG).show();
+
+        login_btn.setEnabled(true);
+    }
+
+    public boolean validate() {
+        boolean valid = true;
+
+        String email = email_text.getText().toString();
+        String password = passwd_text.getText().toString();
+
+        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            email_text.setError("enter a valid email address");
+            valid = false;
+        } else {
+            email_text.setError(null);
+        }
+
+        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+            passwd_text.setError("between 4 and 10 alphanumeric characters");
+            valid = false;
+        } else {
+            passwd_text.setError(null);
+        }
+
+        return valid;
+    }
+}
