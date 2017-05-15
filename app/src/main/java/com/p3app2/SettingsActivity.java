3b06032764ba62efb48f28f3e926a8a68a57aeeb
Kirti V Rathore <kirtivr.bit@gmail.com>
2017-05-15 05:40:36 -0400

    Settings basic stuff done

--- app/src/main/java/com/p3app2/SettingsActivity.java
+++ app/src/main/java/com/p3app2/SettingsActivity.java
@@ -2,17 +2,17 @@ package com.p3app2;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-import android.widget.TextView;
 
 /**
  * Kirti will handle this activity
  */
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends PreferenceActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
     }
 
 }
