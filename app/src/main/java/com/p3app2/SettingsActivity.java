07c13836ac822dca86ccaff83d2a5eabd7a6916f
Kirti V Rathore <kirtivr.bit@gmail.com>
2017-05-17 18:54:13 -0400

    Settings changes

--- app/src/main/java/com/p3app2/SettingsActivity.java
+++ app/src/main/java/com/p3app2/SettingsActivity.java
@@ -1,7 +1,9 @@
 package com.p3app2;
 
 import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -9,6 +11,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
@@ -18,7 +21,7 @@ import android.view.WindowManager;
  * Kirti will handle this activity
  */
 
-public class SettingsActivity extends PreferenceActivity{
+public class SettingsActivity extends PreferenceFragment{
     public static final String KEY_ANON = "setting_anonymous";
     public static final String KEY_UNAME = "setting_username";
     public static final String KEY_MUTE = "setting_mute";
@@ -34,7 +37,7 @@ public class SettingsActivity extends PreferenceActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
-        final SharedPreferences sharedPreferences = getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
+        final SharedPreferences sharedPreferences = getActivity().getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
         // Anon cust
         Preference anon = findPreference(KEY_ANON);
         customizeAnon(anon);
@@ -60,34 +63,8 @@ public class SettingsActivity extends PreferenceActivity{
         customizeScreenshot(ss);
     }
 
-//    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-//        Preference pref = findPreference(key);
-//        Log.d("SOMETHING","SHARED PREF CHANGED");
-//        final SharedPreferences sharedPreferences2 = getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
-//        if (key.equals(KEY_ANON)) {
-//            // Anonymous setting has been changed, do whatever you need to do here
-//            // Maybe change the theme ?
-//
-//            boolean disable = sharedPreferences2.getBoolean(key,false);
-//            if(disable) {
-//                // Disable anonymity
-//                setTheme(R.style.AppTheme_Dark);
-//            } else {
-//                // Enable anonimity
-//                setTheme(R.style.anonTheme);
-//            }
-//        } else if(key.equals(KEY_UNAME)) {
-//            // Username has been changed.
-//            // Maybe chat window should take the new username value.
-//            String uname = sharedPreferences2.getString(key,"null");
-//            pref.setSummary(uname);
-//        } else if(key.equals(KEY_SCREENSHOT)) {
-//
-//        }
-//    }
-
     public void customizeUname(final Preference uname) {
-        final SharedPreferences sharedPreferences3 = getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
+        final SharedPreferences sharedPreferences3 = getActivity().getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
         uname.setSummary(sharedPreferences3.getString(KEY_UNAME,"anon"));
 
         uname.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
@@ -113,26 +90,39 @@ public class SettingsActivity extends PreferenceActivity{
     }
 
     public void customizeContact(Preference contact) {
-        final Context that = this;
-
+        final Context that = getContext();
         contact.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                Intent callIntent = new Intent(Intent.ACTION_CALL);
-                callIntent.setData(Uri.parse("tel:123456789"));
-                if ( ContextCompat.checkSelfPermission( that, Manifest.permission.CALL_PHONE ) != PackageManager.PERMISSION_GRANTED ) {
+                new AlertDialog.Builder(that)
+                        .setTitle("Confirm")
+                        .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // do nothing
+                            }
+                        })
+                        .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // OK has been pressed => force the new value and update the checkbox display
+                                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                                callIntent.setData(Uri.parse("tel:123456789"));
+                                if ( ContextCompat.checkSelfPermission( that, Manifest.permission.CALL_PHONE ) != PackageManager.PERMISSION_GRANTED ) {
+                                    ActivityCompat.requestPermissions(getActivity(), new String[] {  android.Manifest.permission.CALL_PHONE  }, MY_PERMISSIONS_REQUEST_CALL_PHONE);
+                                }
 
-                    ActivityCompat.requestPermissions(new SettingsActivity(), new String[] {  android.Manifest.permission.CALL_PHONE  }, MY_PERMISSIONS_REQUEST_CALL_PHONE);
-                }
+                                startActivity(callIntent);
+                            }
+                        }).create().show();
 
-                startActivity(callIntent);
                 return false;
             }
         });
     }
 
     public void customizeMute(Preference mute) {
-        final SharedPreferences sharedPreferences4 = getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
+        final SharedPreferences sharedPreferences4 = getActivity().getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
         mute.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference mute, Object val) {
@@ -150,15 +140,15 @@ public class SettingsActivity extends PreferenceActivity{
 
 
     public void customizeScreenshot(Preference ss) {
-        final SharedPreferences sharedPreferences5 = getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
+        final SharedPreferences sharedPreferences5 = getActivity().getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
         ss.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference ss, Object val) {
                 Boolean disable = (Boolean)val;
                 if (disable.booleanValue()) {
-                    getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+                    getActivity().getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
                 } else {
-                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+                    getActivity().getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
                 }
                 return true;
             }
@@ -166,17 +156,17 @@ public class SettingsActivity extends PreferenceActivity{
     }
 
     public void customizeAnon(Preference anon) {
-        final SharedPreferences sharedPreferences6 = getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
+        final SharedPreferences sharedPreferences6 = getActivity().getSharedPreferences("Dickshouse", Context.MODE_PRIVATE);
         anon.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(Preference anon, Object val) {
                 Boolean disable = (Boolean)val;
                 if (disable.booleanValue()) {
                     // Disable anonymity
-                    setTheme(R.style.AppTheme_Dark);
+                    getActivity().setTheme(R.style.AppTheme_Dark);
                 } else {
                     // Enable anonimity
-                    setTheme(R.style.anonTheme);
+                    getActivity().setTheme(R.style.anonTheme);
                 }
                 return true;
             }
