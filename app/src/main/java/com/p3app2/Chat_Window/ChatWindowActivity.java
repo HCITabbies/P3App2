8884dccb13603edb1348d026f034096360eca09b
Rohan Arora <rohan0921@gmail.com>
2017-05-19 13:14:15 -0400

    Minor Caption Change

--- app/src/main/java/com/p3app2/Chat_Window/ChatWindowActivity.java
+++ app/src/main/java/com/p3app2/Chat_Window/ChatWindowActivity.java
@@ -56,6 +56,8 @@ public class ChatWindowActivity extends AppCompatActivity {
         } catch (SmackException.NotConnectedException e) {
             e.printStackTrace();
         }
+
+        getSupportActionBar().setTitle("Chat");
     }
 
     private void initControls() throws SmackException.NotConnectedException {
